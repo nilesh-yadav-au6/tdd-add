e19342d8dd3b2e2b5b033cc4ff93589553f010bf
Nilesh Yadav <nileshyadav@Nileshs-MacBook-Pro.local>
2024-06-11 23:02:32 +0530

    updated add function to add numbers

--- src/add.js
+++ src/add.js
@@ -1,7 +1,16 @@
 function add(numbers) {
   if (!numbers) return 0;
 
-  return numbers.reduce((sum, num) => sum + num, 0);
+  let delimiter = /,|\n/;
+  if (numbers.startsWith("//")) {
+    const parts = numbers.split("\n", 2);
+    delimiter = new RegExp(parts[0].slice(2));
+    numbers = parts[1];
+  }
+
+  const numberArray = numbers.split(delimiter).map(Number);
+
+  return numberArray.reduce((sum, num) => sum + num, 0);
 }
 
 module.exports = add;
