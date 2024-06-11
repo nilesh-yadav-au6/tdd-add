2a77e9be9c8c659ef191e809b8c1fc04828290ee
Nilesh Yadav <nileshyadav@Nileshs-MacBook-Pro.local>
2024-06-11 23:08:36 +0530

    updated add function to handle -ve numbers

--- src/add.js
+++ src/add.js
@@ -8,9 +8,16 @@ function add(numbers) {
     numbers = parts[1];
   }
 
-  const numberArray = numbers.split(delimiter).map(Number);
+  const numbersArray = numbers.split(delimiter).map(Number);
+  const negativesNumbers = numbersArray.filter((num) => num < 0);
 
-  return numberArray.reduce((sum, num) => sum + num, 0);
+  if (negativesNumbers.length > 0) {
+    throw new Error(
+      `negative numbers are not allowed: ${negativesNumbers.join(",")}`
+    );
+  }
+
+  return numbersArray.reduce((sum, num) => sum + num, 0);
 }
 
 module.exports = add;
