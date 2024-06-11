4f0735cbff4edc3ca7955c84b0630ca7779afecd
Nilesh Yadav <nileshyadav@Nileshs-MacBook-Pro.local>
2024-06-11 22:55:54 +0530

    Added test case for single number

--- src/test/add.test.js
+++ src/test/add.test.js
@@ -4,4 +4,9 @@ describe("Addition tests", () => {
   test("should return 0 for an empty string", () => {
     expect(add("")).toBe(0);
   });
+
+  test("should return the number itself for single number", () => {
+    expect(add("4")).toBe(4);
+    expect(add("1")).toBe(1);
+  });
 });
